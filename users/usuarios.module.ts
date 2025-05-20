4019c5d911be85617e41538e5e2f54435007be3f
fgomezfranco45 <fgomezfranco45@correo.unicordoba.edu.co>
2025-05-20 12:22:14 +0000

    .

--- users/usuarios.module.ts
+++ users/usuarios.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './schemas/user.schema';
+import { UserSchema } from './schemas/users.schema';
 
 @Module({
   imports :[
