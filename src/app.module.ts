441bd7b51b4ccc14d91711ad82776e2d99491386
fgomezfranco45 <fgomezfranco45@correo.unicordoba.edu.co>
2025-05-20 12:44:06 +0000

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsuariosModule } from './users/usuarios.module';
+import { TareasModule } from './tareas/tareas.module';
+import { QuicesModule } from './quices/quices.module';
+import { EvaluacionesModule } from './evaluaciones/evaluaciones.module';
+import { EntregasModule } from './entregas/entregas.module';
+import { ForosModule } from './foros/foros.module';
+import { ModulosModule } from './modulos/modulos.module';
+import { ArchivosModule } from './archivos/archivos.module';
 
 @Module({
   imports: [
+    UsuariosModule,
+    ModulosModule,
+    ArchivosModule,
+    EntregasModule,
+    TareasModule,
+    QuicesModule,
+    EvaluacionesModule,
+    ForosModule,
     ConfigModule.forRoot({
     isGlobal: true,
   }),
